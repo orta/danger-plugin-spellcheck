1a7a33e93509af07f97aea87ce932e5e7cc30293
Elan Ruusamäe <glen@delfi.ee>
2022-03-21 23:15:29 +0200

    Add fileLinks method

--- src/repository/github.ts
+++ src/repository/github.ts
@@ -2,4 +2,8 @@ import { GitHubDSL } from "danger"
 
 export class GitHubProvider {
   public constructor(private readonly api: GitHubDSL) {}
+
+  public fileLinks(paths: string[]) {
+    return this.api.utils.fileLinks(paths)
+  }
 }
